1d4b41c2edfa86976e6d2a9230624fcb6dc3cc44
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2022-12-22 16:24:03 +0800

    test: make sample test

---
+++ t/sample.js
@@ -0,0 +1,8 @@
+var assert = require("assert");
+describe("Operation", function() {
+	describe("Addition", function() {
+		it("should return 2", function() {
+			assert.equal(1+1, 2);
+		});
+	});
+});
